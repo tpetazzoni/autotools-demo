b1cc58938e529c21efcd2672fb577c8e82931345
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2016-03-27 19:52:27 +0200

    Fifteenth demo
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- src/hello.c
+++ src/hello.c
@@ -1,13 +1,10 @@
 #include <dlfcn.h>
-#include <curl/curl.h>
 #include "common.h"
 
 int main(void)
 {
 	void *lib;
-	CURL *c;
 	say("Hello World");
 	lib = dlopen("foo.so", RTLD_NOW);
-	c = curl_easy_init();
 	return 0;
 }
