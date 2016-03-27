c726bab76ebd94416c7b4682840765abdc48c5a4
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2016-03-27 20:14:41 +0200

    Seventeenth demo
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- src/common.h
+++ src/common.h
@@ -1,6 +1,14 @@
 #ifndef COMMON_H
 #define COMMON_H
 
+#include "config.h"
+
 void say(const char *str);
+
+#if defined(DEBUG_ENABLED)
+void debug(void);
+#else
+static inline void debug(void) {}
+#endif
 
 #endif
---
+++ src/debug.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+void debug(void)
+{
+	printf("This is the debug function\n");
+}
