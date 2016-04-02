ad9391db8e1cdefd05ac6dd4bdaf1cd663a7c797
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2016-04-02 15:53:46 +0200

    Nineteenth demo
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- src/hello.c
+++ src/hello.c
@@ -1,10 +1,14 @@
 #include <dlfcn.h>
+#include <demo.h>
 #include "common.h"
 
 int main(void)
 {
 	void *lib;
 	say("Hello World");
+#if defined(DEBUG_ENABLED)
+	debug();
+#endif
 	lib = dlopen("foo.so", RTLD_NOW);
 	return 0;
 }
